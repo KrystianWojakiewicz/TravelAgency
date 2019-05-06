01a905c340b54e8f2d0f00b44967816a8c58cdf6
krysh97 <krysh97@gmail.com>
2019-05-06 03:37:51 +0200

    Added Registration and Login Windows Implemented initial Registration logic

---
+++ TravelAgency/Register.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TravelAgency
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+
+        private void addNewUser(TravelAgencyDBEntities db)
+        {
+            if (!String.IsNullOrEmpty(usernameTextBox.Text) && !String.IsNullOrEmpty(passwordTextBox.Text))
+            {
+                User newUser = new User()
+                {
+                    UserID = 1212,
+                    Username = usernameTextBox.Text,
+                    Password = passwordTextBox.Text,
+                    Saldo = 0
+                };
+                db.User.Add(newUser);
+                db.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("Please enter your credentials");
+            }
+ 
+            MessageBox.Show("User has been successfully registered");
+        }
+
+        private void clearInputBoxes()
+        {
+            usernameTextBox.Clear();
+            passwordTextBox.Clear();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            TravelAgencyDBEntities db = new TravelAgencyDBEntities();
+            addNewUser(db);
+            clearInputBoxes();
+
+            this.Close();
+            App.Current.MainWindow.Show();
+        }
+    }
+}
